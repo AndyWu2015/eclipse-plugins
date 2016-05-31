52b32c53db4ddb949dd7b03629c1084f3b9716ca
Andy Wu <andy.wu@liferay.com>
2016-05-31 09:41:45 +0800

    revert RCPClock

---
+++ RCPClock/src/rcpclock/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,20 @@
+package rcpclock;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "RCPClock.perspective";
+
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+}
